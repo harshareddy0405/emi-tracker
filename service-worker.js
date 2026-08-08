3051fb871bcf8d6d949d0e4143790c8ede7d1bf4
Harsha Reddy <harshareddy0405@users.noreply.github.com>
2026-08-08 13:55:52 +0530

    Meet mobile search touch target sizing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "emi-tracker-v23";
+const CACHE = "emi-tracker-v24";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", event => {
